80b1ef96ac0aaee471fc7b447f58e4dffb30013c
diamenciarz <40736778+diamenciarz@users.noreply.github.com>
2018-11-07 18:53:40 +0100

    Update index.js

--- degnoming/index.js
+++ degnoming/index.js
@@ -1,4 +1,6 @@
 function arrow() {
+	
+	var arrowNumberDirection = 1;
 
 	for (var i = 0; i < 1000; i++) {
 		var random = Math.ceil(Math.random() * 100);
@@ -10,45 +12,64 @@ function arrow() {
 	console.log(random)
 	if (length == 1) {
 		if (random <= 25) {
-			arrowDirection = '&#11016;';
+			arrowNumberDirection = 8;
 		} else if (random <= 75) {
-			arrowDirection = '&#11014;'
+			arrowNumberDirection = 1;
 		} else if (random <= 100) {
-			arrowDirection = '&#11017;';
+			arrowNumberDirection = 2;
 		}
 	}
 	if (length == 2) {
 		if (random <= 10) {
-			arrowDirection = '&#10145;';
+			arrowNumberDirection = 7;
 		} else if (random <= 30) {
-			arrowDirection = '&#11016;';
+			arrowNumberDirection = 8;
 		} else if (random <= 70) {
-			arrowDirection = '&#11014;'
+			arrowNumberDirection = 1;
 		} else if (random <= 90) {
-			arrowDirection = '&#11017;';
+			arrowNumberDirection = 2;
 		} else if (random <= 100) {
-			arrowDirection = '&#11013;';
+			arrowNumberDirection = 3;
 		}
 	}
 	if (length == 3) {
 		if (random <= 2) {
-			arrowDirection = '&#11015;';
+			arrowNumberDirection = 5;
 		} else if (random <= 8) {
-			arrowDirection = '&#11018;';
+			arrowNumberDirection = 6;
 		} else if (random <= 21) {
-			arrowDirection = '&#10145;';
+			arrowNumberDirection = 7;
 		} else if (random <= 36) {
-			arrowDirection = '&#11016;';
+			arrowNumberDirection = 8;
 		} else if (random <= 66) {
-			arrowDirection = '&#11014;'
+			arrowNumberDirection = 1;
 		} else if (random <= 81) {
-			arrowDirection = '&#11017;';
+			arrowNumberDirection = 2;
 		} else if (random <= 94) {
-			arrowDirection = '&#11013;';
+			arrowNumberDirection = 3;
 		} else if (random <= 100) {
-			arrowDirection = '&#11019;';
+			arrowNumberDirection = 4;
 		}
 	}
+	
+	if (arrowNumberDirection == 5) {
+			arrowDirection = '&#11015;';
+		} else if (arrowNumberDirection == 6) {
+			arrowDirection = '&#11018;';
+		} else if (arrowNumberDirection == 7) {
+			arrowDirection = '&#10145;';
+		} else if (arrowNumberDirection == 8) {
+			arrowDirection = '&#11016;';
+		} else if (arrowNumberDirection == 1) {
+			arrowDirection = '&#11014;'
+		} else if (arrowNumberDirection == 2) {
+			arrowDirection = '&#11017;';
+		} else if (arrowNumberDirection == 3) {
+			arrowDirection = '&#11013;';
+		} else if (arrowNumberDirection == 4) {
+			arrowDirection = '&#11019;';
+		}
+	
 	console.log(arrowDirection);
 	$('.display').html('');
 	$('#result' + this.dataset.number).html(arrowDirection);
